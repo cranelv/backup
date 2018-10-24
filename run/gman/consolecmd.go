1971c3a32f37e030864ff64d40dc53f409f8003d
root <915716144@qq.com>
2018-10-24 17:11:00 +0800

    revert

--- run/gman/consolecmd.go
+++ run/gman/consolecmd.go
@@ -1,21 +1,6 @@
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or or http://www.opensource.org/licenses/mit-license.php
-// Copyright 2016 The go-matrix Authors
-// This file is part of go-matrix.
-//
-// go-matrix is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// go-matrix is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with go-matrix. If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
