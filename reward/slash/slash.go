ec91deb776f3b6e1b219e2bdda1937407e8a2710
zhangwen <sdhczw@163.com>
2018-11-27 17:39:38 +0800

    惩罚判断利息的3/4

--- reward/slash/slash.go
+++ reward/slash/slash.go
@@ -96,7 +96,7 @@ func (bp *BlockSlash) CalcSlash(currentState *state.StateDB, num uint64) map[com
 			log.WARN(PackageName, "获取奖励错误，账户", v.Account)
 			continue
 		}
-		accountReward:=new(big.Int).Sub(currentAccountReward,preAccountReward)
+		accountReward := new(big.Int).Sub(currentAccountReward, preAccountReward)
 		if 0 == accountReward.Cmp(new(big.Int).SetUint64(0)) {
 			log.WARN(PackageName, "获取奖励值为0，账户", v.Account)
 			continue
@@ -114,6 +114,10 @@ func (bp *BlockSlash) CalcSlash(currentState *state.StateDB, num uint64) map[com
 		}
 
 		temp := 1 - float64(currentOnlineTime.Uint64()-preOnlineTime.Uint64())/float64(bp.eleMaxOnlineTime)
+
+		if temp >= 0.75 {
+			temp = 0.75
+		}
 		slash := new(big.Int).SetUint64(uint64(float64(accountReward.Uint64()) * temp))
 		log.INFO(PackageName, "账户", v.Account.String(), "惩罚金额", accountReward, "slash", slash)
 		depoistInfo.AddSlash(currentState, v.Account, slash)
