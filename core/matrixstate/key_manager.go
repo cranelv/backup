2d44efac28f55ff0c0369a6a5de33c8419414f51
huyuankai <huyuankai@vip.qq.com>
2018-12-08 10:56:48 +0800

    解决panic错误

--- core/matrixstate/key_manager.go
+++ core/matrixstate/key_manager.go
@@ -63,6 +63,7 @@ func newKeyManager() *keyManager {
 			MSPElectGraph:        types.RlpHash(matrixStatePrefix + MSPElectGraph),
 			MSPElectOnlineState:  types.RlpHash(matrixStatePrefix + MSPElectOnlineState),
 		},
+		codecMap: make(map[string]codec),
 	}
 	km.initCodec()
 	return km
