02ab9d02ed9c9ba3f242152e1f75059ea6dbfe13
huyuankai <huyuankai@vip.qq.com>
2019-01-15 13:51:20 +0800

    调整格式

--- params/enstrust/entrust.go
+++ params/enstrust/entrust.go
@@ -2,12 +2,12 @@ package entrust
 
 import (
 	"errors"
+	"fmt"
 	"github.com/matrix/go-matrix/accounts"
+	"github.com/matrix/go-matrix/base58"
 	"github.com/matrix/go-matrix/common"
 	"github.com/matrix/go-matrix/log"
 	"sync"
-	"fmt"
-	"github.com/matrix/go-matrix/base58"
 )
 
 type EntrustValue struct {
@@ -28,11 +28,11 @@ var (
 func (self *EntrustValue) SetEntrustValue(data map[common.Address]string) error {
 	self.mu.RLock()
 	defer self.mu.RUnlock()
-	entrustData,noEntrustAccounts,flag := VerifyA2AccountAndPassword(data)
+	entrustData, noEntrustAccounts, flag := VerifyA2AccountAndPassword(data)
 	if !flag {
 		return errors.New(noEntrustAccounts)
 	}
-	for account,password := range entrustData{
+	for account, password := range entrustData {
 		self.entrustValue[account] = password
 	}
 	return nil
@@ -44,7 +44,7 @@ func (self *EntrustValue) GetEntrustValue() map[common.Address]string {
 }
 
 type AccountChecker interface {
-	CheckAccountAndPassword(a accounts.Account, passphrase string)  error
+	CheckAccountAndPassword(a accounts.Account, passphrase string) error
 }
 
 func SetAccountChecker(checker AccountChecker) {
@@ -53,9 +53,9 @@ func SetAccountChecker(checker AccountChecker) {
 
 var accountChecker AccountChecker
 
-func VerifyA2AccountAndPassword(data map[common.Address]string) (map[common.Address]string,string, bool) {
+func VerifyA2AccountAndPassword(data map[common.Address]string) (map[common.Address]string, string, bool) {
 	if accountChecker == nil {
-		log.Error("验证A2账户密码","检查器未设置","检查器 is nil")
+		log.Error("验证A2账户密码", "检查器未设置", "检查器 is nil")
 		return nil, "", false
 	}
 
@@ -65,13 +65,13 @@ func VerifyA2AccountAndPassword(data map[common.Address]string) (map[common.Addr
 	for address, password := range data {
 		err := accountChecker.CheckAccountAndPassword(accounts.Account{Address: address}, password)
 		if err != nil {
-			noEntrustAccounts += fmt.Sprintf("%s\n",base58.Base58EncodeToString("MAN",address))
-			log.Warn("验证A2账户密码", "验证未通过的账户", base58.Base58EncodeToString("MAN",address), "password", password)
+			noEntrustAccounts += fmt.Sprintf("%s\n", base58.Base58EncodeToString("MAN", address))
+			log.Warn("验证A2账户密码", "验证未通过的账户", base58.Base58EncodeToString("MAN", address), "password", password)
 			flag = false
 			continue
 		}
-		log.Debug("验证A2账户密码", "验证通过的账户", base58.Base58EncodeToString("MAN",address))
+		log.Debug("验证A2账户密码", "验证通过的账户", base58.Base58EncodeToString("MAN", address))
 		entrustData[address] = password
 	}
-	return entrustData,noEntrustAccounts, flag
+	return entrustData, noEntrustAccounts, flag
 }
