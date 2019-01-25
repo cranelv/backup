3c56213423a2901de1d8c9894e337331be9589ab
huyuankai <huyuankai@vip.qq.com>
2019-01-25 19:21:57 +0800

    修改跑交易失败，导致panic的bug

--- blkverify/verify_process.go
+++ blkverify/verify_process.go
@@ -506,12 +506,12 @@ func (p *Process) StartVerifyTxsAndState(result *core.RetChan) {
 }
 
 func (p *Process) verifyTxsAndState() {
-
 	log.Trace(p.logExtraInfo(), "开始交易验证, 数量", len(p.curProcessReq.originalTxs), "高度", p.number)
 	stateDB, finalTxs, receipts, _, err := p.pm.manblk.VerifyTxsAndState(blkmanage.CommonBlk, string(p.curProcessReq.req.Header.Version), p.curProcessReq.req.Header, p.curProcessReq.originalTxs, nil)
 	if nil != err {
 		log.Error(p.logExtraInfo(), "交易及状态验证失败", err, "高度", p.number, "req leader", p.curProcessReq.req.Header.Leader.Hex())
 		p.startDPOSVerify(localVerifyResultStateFailed)
+		return
 	}
 
 	p.curProcessReq.stateDB, p.curProcessReq.finalTxs, p.curProcessReq.receipts = stateDB, finalTxs, receipts
