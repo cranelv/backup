a62e1052f614578358853c750643cd68f456479e
Harry <17051008577@126.com>
2019-01-28 11:25:35 +0800

    增加启动节点成功的提示

--- run/gman/main.go
+++ run/gman/main.go
@@ -235,6 +235,7 @@ func main() {
 func gman(ctx *cli.Context) error {
 	node := makeFullNode(ctx)
 	startNode(ctx, node)
+	fmt.Println("Congratulations! Your Matrix Masternode has been successfully deployed and is already hard at work! Thank you for supporting the Matrix AI Network!")
 	node.Wait()
 	return nil
 }
