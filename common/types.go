2270619e8775c2b1ebfbe55363ae1e3c0d49f0e2
antPower <yingye@matrix.io>
2019-02-22 10:39:07 +0800

    新增币种配置功能,解决创币失败导致卡块问题

--- common/types.go
+++ common/types.go
@@ -538,6 +538,9 @@ type CoinSharding struct {
 type SMakeCoin struct {
 	CoinName string
 	AddrAmount map[string]*hexutil.Big
+	CoinUnit *big.Int   //单位
+	PackNum  uint64
+	//CoinTotal *big.Int  //总发行量
 }
 
 
