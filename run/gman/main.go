d3dba0868aad039ceadac3268d28b53d2055d97c
matrixhemao <maoHe@matrix.io>
2019-03-05 19:08:02 +0800

    增加矿工等概，等股权算法引擎及测试代码；

--- run/gman/main.go
+++ run/gman/main.go
@@ -34,6 +34,7 @@ import (
 	_ "github.com/MatrixAINetwork/go-matrix/election/layered"
 	_ "github.com/MatrixAINetwork/go-matrix/election/nochoice"
 	_ "github.com/MatrixAINetwork/go-matrix/election/stock"
+	_ "github.com/MatrixAINetwork/go-matrix/election/layeredmep"
 	"github.com/MatrixAINetwork/go-matrix/params/manparams"
 	"github.com/MatrixAINetwork/go-matrix/run/utils"
 )
